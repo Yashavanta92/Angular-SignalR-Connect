386445fc4985cb2d314807a7bf4a3852be4c651b
Yashavanta Byagawadi <39549731+Yashavanta92@users.noreply.github.com>
2022-05-05 12:10:01 +0530

    Locally generating access token.

--- src/app/services/signal-r.service.ts
+++ src/app/services/signal-r.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { HubConnectionBuilder, HubConnection } from "@microsoft/signalr";
 import { environment } from 'src/environments/environment';
 
+import * as CryptoJS from 'crypto-js';
+
 interface INegotiateResult {
   accessToken: string;
   url: string;
@@ -24,27 +26,88 @@ export class SignalRService {
   getSignalRHubConnection(accountNumber: string, premiseId: string) {
     console.log('accountNumber - ', accountNumber, 'premiseId - ', premiseId);
 
-    this.initiateNegotiation().subscribe(signalRConn => {
-      const hubConnection: HubConnection = new HubConnectionBuilder()
-        .withUrl(signalRConn.url, { accessTokenFactory: () => signalRConn.accessToken })
-        .build();
+    // this.initiateNegotiation().subscribe(signalRConn => {
+    const hubConnection: HubConnection = new HubConnectionBuilder()
+      .withUrl('https://processoutages.azurewebsites.net/api', { accessTokenFactory: () => { return this.generateAccessToken('yashvanta') } })
+      .build();
 
-      hubConnection.start().then((success) => {
-        console.log('SignalR connection started');
-        hubConnection.invoke('outageTracker', accountNumber, premiseId);
-      }).catch((error) => {
-        console.log('Error in starting the hub connection ', error);
-      });
+    hubConnection.start().then(() => {
+      console.log('SignalR connection started');
 
-      hubConnection.on('broadcastOutageData', (outageData) => {
-        console.log('SignalR broadcasted data');
+      hubConnection.invoke('GetOutageData', accountNumber, premiseId).then((outageData) => {
         this.outageData$.next(outageData);
+      }).catch((error) => {
+        console.log('Error in invoking the GetOutageData ', error);
       });
 
-      hubConnection.onclose(() => {
-        console.log('SignalR connection closed');
+      hubConnection.invoke('SignalRTest', accountNumber, premiseId).then((msg) => {
+        this.outageData$.next(msg);
+      }).catch((error) => {
+        console.log('Error in invoking the SignalRTest ', error);
       });
+
+    }).catch((error) => {
+      console.log('Error in starting the hub connection ', error);
     });
+
+    hubConnection.on('broadcastOutageData', (outageData) => {
+      console.log('SignalR broadcasted data');
+      this.outageData$.next(outageData);
+    });
+
+    hubConnection.on('ReceiveMessage', (outageData) => {
+      console.log('SignalR ReceiveMessage data', outageData);
+      this.outageData$.next(outageData);
+    });
+
+    hubConnection.onclose(() => {
+      console.log('SignalR connection closed');
+    });
+    // });
   }
 
+  generateAccessToken(userName = 'Yashavanta') {
+    var header = {
+      "alg": "HS256",
+      "typ": "JWT"
+    };
+
+    const stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+    var encodedHeader = this.base64url(stringifiedHeader);
+
+    // customize your JWT token payload here 
+    var data = {
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": userName,
+      "exp": 1699819025,
+      // 'admin': isAdmin
+    };
+
+    var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
+    var encodedData = this.base64url(stringifiedData);
+
+    var token = encodedHeader + "." + encodedData;
+
+    var secret = "myfunctionauthtest"; // do not expose your secret here
+
+    var signature: CryptoJS.lib.WordArray = CryptoJS.HmacSHA256(token, secret);
+    var signatureStr = this.base64url(signature);
+
+    var signedToken = token + "." + signatureStr;
+
+    return signedToken;
+  }
+
+  base64url(source: CryptoJS.lib.WordArray) {
+    // Encode in classical base64
+    let encodedSource = CryptoJS.enc.Base64.stringify(source);
+
+    // Remove padding equal characters
+    encodedSource = encodedSource.replace(/=+$/, '');
+
+    // Replace characters according to base64url specifications
+    encodedSource = encodedSource.replace(/\+/g, '-');
+    encodedSource = encodedSource.replace(/\//g, '_');
+
+    return encodedSource;
+  }
 }
